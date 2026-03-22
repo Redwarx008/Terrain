97cf16c349155fc1550cbb15c8b0b32ac63eae0a
Redwarx008 <Redwarx008@outlook.com>
2026-03-22 22:42:29 +0800

    关闭 VSync 并显示帧率

--- Terrain.Windows/TerrainApp.cs
+++ Terrain.Windows/TerrainApp.cs
@@ -1,5 +1,28 @@
 using Stride.Engine;
+using Stride.Games;
+using Stride.Profiling;
 
-using var game = new Game();
+using var game = new TerrainGame();
 game.Run();
 
+/// <summary>
+/// 自定义游戏类，用于配置图形设置。
+/// </summary>
+public class TerrainGame : Game
+{
+    protected override void BeginRun()
+    {
+        base.BeginRun();
+        
+        // 关闭 VSync 以获得更高的帧率
+        var graphicsDeviceManager = Services.GetService<IGraphicsDeviceManager>() as GraphicsDeviceManager;
+        if (graphicsDeviceManager != null)
+        {
+            graphicsDeviceManager.SynchronizeWithVerticalRetrace = false;
+            graphicsDeviceManager.ApplyChanges();
+        }
+        
+        // 显示帧率和性能信息
+        ProfilingSystem.EnableProfiling(false, GameProfilingKeys.GameDrawFPS);
+    }
+}
