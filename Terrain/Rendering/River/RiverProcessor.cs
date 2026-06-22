cc258d586a408cd76dece49c78bc07541d8a0cd1
Redwarx008 <Redwarx008@outlook.com>
2026-06-23 05:46:29 +0800

    Refactor river settings binding

--- Terrain/Rendering/River/RiverProcessor.cs
+++ Terrain/Rendering/River/RiverProcessor.cs
@@ -63,7 +63,6 @@ public sealed class RiverProcessor : EntityProcessor<RiverComponent, RiverProces
         bool enabled = component.Enabled && component.Settings.Visible;
         foreach (var renderObject in data.RenderObjects)
         {
-            renderObject.ApplySettings(component.Settings);
             renderObject.Enabled = enabled;
             renderObject.RenderGroup = RiverRenderGroups.RiverRenderGroup;
             renderObject.World = entity.Transform.WorldMatrix;
